c2b6baff80d23b0d1fa4adebb9be9c5539e7dfd2
Princi Mixtiy <princi03f@gmail.com>
2024-09-21 12:30:58 +0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'index.html',
+    'html/*.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#00ff00',
+      }
+    },
+  },
+  plugins: [],
+}
